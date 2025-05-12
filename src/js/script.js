9a6ffc2a31dcdf378017bb2b34367dddb427f8b6
Caio Poci <caioalezp@gmail.com>
2025-05-12 20:17:01 -0300

    js atualizacao, loop, preenchimento, e resumo

--- src/js/script.js
+++ src/js/script.js
@@ -39,3 +39,44 @@ let safra1, safra2, safra3, safra4, safra5;
 let quant1, quant2, quant3, quant4, quant5;
 
 let continuar = "sim";
+
+while (continuar.toLowerCase() === "sim" && contador < 5) {
+    contador++;
+
+    let nome = validarEntrada("Digite o nome do vinho:");
+    let safra = parseInt(validarEntrada("Digite o ano da safra:"));
+    let quantidade = parseInt(validarEntrada("Digite a quantidade em estoque:"));
+
+    let classificacao = classificarVinho(safra);
+    mostrarDados(nome, safra, quantidade, classificacao);
+
+    if (estoqueBaixo(quantidade)) {
+        estoqueBaixoCont++;
+    }
+
+    if (safra < menorAno) {
+        menorAno = safra;
+        nomeMaisAntigo = nome;
+    }
+
+    if (contador === 1) {
+        nome1 = nome; safra1 = safra; quant1 = quantidade;
+    } else if (contador === 2) {
+        nome2 = nome; safra2 = safra; quant2 = quantidade;
+    } else if (contador === 3) {
+        nome3 = nome; safra3 = safra; quant3 = quantidade;
+    } else if (contador === 4) {
+        nome4 = nome; safra4 = safra; quant4 = quantidade;
+    } else if (contador === 5) {
+        nome5 = nome; safra5 = safra; quant5 = quantidade;
+    }
+
+    continuar = prompt("Deseja cadastrar outro vinho? (sim/não)");
+}
+
+alert(
+    "Cadastro finalizado!\n" +
+    "Total de cadastros: " + contador + "\n" +
+    "Vinhos com estoque baixo: " + estoqueBaixoCont + "\n" +
+    "Vinho mais antigo: " + nomeMaisAntigo + " (Safra: " + menorAno + ")"
+);
